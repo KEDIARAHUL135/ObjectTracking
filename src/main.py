e77ec6323e69c8d9ab727a99284e186d0fc37e07
KEDIARAHUL135 <rahul.kedia@airavath.in>
2020-01-20 10:13:05 +0530

    Update documentation

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+###############################################################################
+# File          : main.py
+# Created by    : Rahul Kedia
+# Created on    : 19/01/2020
+# Project       : ObjectTracking
+# Description   : This file contains the main source code for the project.
+################################################################################
+
 import cv2
 import numpy as np
 import src.macros as M
@@ -5,6 +13,7 @@ import time
 
 t1 = time.time()
 
+# Creating object for reading video as input
 global cap
 if M.VIDEO_OR_WEBCAM == 1:
     cap = cv2.VideoCapture(M.INPUT_VIDEO_PATH)
@@ -14,6 +23,13 @@ else:
 _, OldFrame = cap.read()
 
 
+################################################################################
+# Function      : DisplayFrames
+# Parameter     : {All the parameters are clearly mentioned and contains
+#                 the images which are to be shown.}
+# Description   : This function shows images as required.
+# Return        : -
+################################################################################
 def DisplayFrames(NewFrame, DiffFrame, ThreshDiffFrame, DilateDiffFrame, Frame):
 
     if M.SHOW_ALLorOUTPUT == 1:
@@ -31,20 +47,25 @@ while cap.isOpened():
     if ret is False:
         break
 
+    # Copying frame to print output on it. This is done so that frame to be processed is not disturbed.
     Frame = NewFrame.copy()
 
+    # Implementing different operations to get image with white area in which movement has occured.
     DiffFrame = cv2.absdiff(OldFrame, NewFrame)
     BlurDiffFrame = cv2.GaussianBlur(DiffFrame, (5, 5), 2)
     GrayDiffFrame = cv2.cvtColor(BlurDiffFrame, cv2.COLOR_BGR2GRAY)
     _, ThreshDiffFrame = cv2.threshold(GrayDiffFrame, M.THRESHOLD_AT, 255, cv2.THRESH_BINARY)
     DilateDiffFrame = cv2.dilate(ThreshDiffFrame, (11, 11), iterations=M.DILATION_ITERATIONS)
 
+    # Finding contours of movement
     Contours, _ = cv2.findContours(DilateDiffFrame, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
 
     for Contour in Contours:
+        # too small contours of movement/noise is rejected.
         if cv2.contourArea(Contour) < M.MIN_CONTOUR_AREA:
             continue
 
+        # Creating rectangle bounding the contour.
         (x, y, w, h) = cv2.boundingRect(Contour)
         cv2.rectangle(Frame, (x, y), (x+w, y+h), (0, 0, 255), thickness=2)
 
